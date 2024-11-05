cd3918947e7bb32738de6c991b57ea021d0f467d
o3010a7 <o3010a7@gmail.com>
2024-11-05 12:28:28 +0400

    added styling and fixed cassette movement

--- js/script.js
+++ js/script.js
@@ -24,6 +24,18 @@ function dragElement(elmnt) {
     document.onmousemove = elementDrag;
   }
 
+  // function elementDrag(e) {
+  //   e = e || window.event;
+  //   e.preventDefault();
+  //   // calculate the new cursor position:
+  //   pos1 = pos3 - e.clientX;
+  //   pos2 = pos4 - e.clientY;
+  //   pos3 = e.clientX;
+  //   pos4 = e.clientY;
+  //   // set the element's new position:
+  //   elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+  //   elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  // }
   function elementDrag(e) {
     e = e || window.event;
     e.preventDefault();
@@ -32,9 +44,30 @@ function dragElement(elmnt) {
     pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
     pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
-    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+
+    // Set the new position, ensuring it's within viewport boundaries
+    var newTop = elmnt.offsetTop - pos2;
+    var newLeft = elmnt.offsetLeft - pos1;
+
+    // Get viewport dimensions
+    var viewportWidth = window.innerWidth;
+    var viewportHeight = window.innerHeight;
+
+    // Get element dimensions
+    var elmntWidth = elmnt.offsetWidth;
+    var elmntHeight = elmnt.offsetHeight;
+
+    // Boundary checks
+    if (newLeft < 0) newLeft = 0;
+    if (newTop < 0) newTop = 0;
+    if (newLeft + elmntWidth > viewportWidth)
+      newLeft = viewportWidth - elmntWidth;
+    if (newTop + elmntHeight > viewportHeight)
+      newTop = viewportHeight - elmntHeight;
+
+    // Set the element's new position:
+    elmnt.style.top = newTop + "px";
+    elmnt.style.left = newLeft + "px";
   }
 
   function closeDragElement() {
